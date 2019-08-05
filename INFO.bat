abe8ef57f6c77b2d699bfcdab045c2a22ca75c5c
lagalapizza_1011 <52937131+SH0-ahacker@users.noreply.github.com>
2019-08-04 21:40:34 -0300

    Create INFO.bat

---
+++ INFO.bat
@@ -0,0 +1,10 @@
+@echo off
+TITLE YOU ARE AWESOME - INFO
+ECHO: HELLO!
+PAUSE
+CLS
+ECHO: THE "YOU ARE AWEESOME" HAVE BEEN CREATED BY:MARCOS
+PAUSE
+CLS
+ECHO: RUNNING...
+start https://github.com/SH0-ahacker/you_are_awesome/
